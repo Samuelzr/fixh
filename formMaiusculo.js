3b82cae54ba52df08b549a1b4f652854d9036498
Samuelzr <111665246+Samuelzr@users.noreply.github.com>
2023-08-21 08:34:46 -0300

    Delete formMaiusculo.js

--- formMaiusculo.js
+++
@@ -1,7 +0,0 @@
-/*funcao para deixar formulario maiusculo*/
-$(document).ready(function maiuscula() {
-
-    $(".maiuscula").keyup(function(){
-        $(this).val($(this).val().toUpperCase());
-      });
-      });
